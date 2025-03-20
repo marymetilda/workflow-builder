c394564b4f65a8033ad54e9ff9e7c88bb525a5d7
marymetilda <metildaphy@gmail.com>
2025-03-20 20:42:57 +0530

    feat: update the sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,26 @@
 import React from "react";
+import { LiaHourglassStartSolid } from "react-icons/lia";
+import { FcProcess } from "react-icons/fc";
+import { TbAirConditioning } from "react-icons/tb";
+import { GiFinishLine } from "react-icons/gi";
 
 const nodeTypes = [
-  { type: "process", label: "Process" },
-  { type: "decision", label: "Decision" },
+  {
+    type: "start",
+    label: "Start",
+    icon: <LiaHourglassStartSolid className="text-green-700" />,
+  },
+  { type: "process", label: "Process", icon: <FcProcess /> },
+  {
+    type: "decision",
+    label: "Decision",
+    icon: <TbAirConditioning className="text-yellow-600" />,
+  },
+  {
+    type: "end",
+    label: "End",
+    icon: <GiFinishLine className="text-pink-500" />,
+  },
 ];
 
 export default function Sidebar() {
@@ -17,10 +35,11 @@ export default function Sidebar() {
       {nodeTypes.map((node) => (
         <div
           key={node.type}
-          className="p-2 mb-2 bg-white border rounded cursor-grab"
+          className="p-2 mb-2 bg-white border rounded cursor-grab flex items-center justify-center border-blue-500"
           draggable
           onDragStart={(event) => onDragStart(event, node.type)}
         >
+          {node.icon && node.icon}
           {node.label}
         </div>
       ))}
